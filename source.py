cb0e78cb6b61345234d2f920dc8a0defe2ef3b19
K.Udul Dewmina <165687248+KUDewmina@users.noreply.github.com>
2025-03-16 19:05:28 +0530

    Update source.py

--- source.py
+++ source.py
@@ -3,12 +3,13 @@ class Solution:
         left,right = 0,len(height)-1  #initializing two poiters and creating variable "max_water"
         max_water = 0 
         while left < right:  #repeating array
+            #calculating current area
             width = right-left
             min_height = min(height[left],height[right])
             area = width*min_height
             max_water=max(area,max_water)
 
-            if height[left]<height[right]:  
+            if height[left]<height[right]:  # Move the pointer pointing to the shorter height
                 left+=1
             else:
                 right-=1
